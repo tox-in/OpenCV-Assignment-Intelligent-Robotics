0dedd7bc040442570c6a663df062c146b0acdce2
tox-in <habimanat78@gmail.com>
2025-02-02 17:40:44 +0200

    question 1 codes

---
+++ q1(decoding_image-01).py
@@ -0,0 +1,5 @@
+from aspose.barcode import barcoderecognition
+reader = barcoderecognition.BarCodeReader("image01.png", barcoderecognition.DecodeType.ALL_SUPPORTED_TYPES)
+recognized_results = reader.read_bar_codes()
+for barcode in recognized_results:
+    print(barcode.code_text)
