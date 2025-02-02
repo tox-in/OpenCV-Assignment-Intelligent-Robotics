bc6cb9405adb2d7a05fc6971081f29e43f2f8a71
tox-in <habimanat78@gmail.com>
2025-02-02 17:45:33 +0200

    question 2 codes

---
+++ q2(decoding_image-02).py
@@ -0,0 +1,21 @@
+from pylibdmtx.pylibdmtx import decode
+import cv2
+
+
+image = cv2.imread("image02.png", cv2.IMREAD_GRAYSCALE) 
+
+
+decoded_objects = decode(image)
+
+for obj in decoded_objects:
+    data = obj.data.decode("utf-8")
+    print("Decoded Data:", data)
+
+
+output_file = "decoded_datamatrix_code.png"
+cv2.imwrite(output_file, image)
+print(f" Annotated image saved as {output_file}")
+
+cv2.imshow("Barcode with Annotation", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
